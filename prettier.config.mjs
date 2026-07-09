17caa27702aec88855e00e9d6e29dc0d9dcbc9c7
hubcio2115 <kowalski.hubercik1@gmail.com>
2026-07-09 12:34:13 +0200

    converted .prettierc to prettier.config.mjs

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/** @import { Config } from "prettier" */
+
+/** @type {Config} */
+const config = {
+  singleQuote: false,
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  printWidth: 120,
+}
+
+export default config
