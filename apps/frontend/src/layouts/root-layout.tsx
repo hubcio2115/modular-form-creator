d1593aaa4f9462cfa11c855e223385135a29b7f8
hubcio2115 <kowalski.hubercik1@gmail.com>
2026-07-10 14:03:46 +0200

    added NuqsAdapter for SearchParams

--- apps/frontend/src/layouts/root-layout.tsx
+++ apps/frontend/src/layouts/root-layout.tsx
@@ -1,6 +1,7 @@
 import { GlobalStyles, theme } from "@components/design-system";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { NuqsAdapter } from "nuqs/adapters/react-router/v8";
 import { ThemeProvider } from "styled-components";
 import { Outlet } from "react-router";
 import { queryClient } from "~/lib/query-client";
@@ -9,8 +10,10 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Outlet />
+        <NuqsAdapter>
+          <GlobalStyles />
+          <Outlet />
+        </NuqsAdapter>
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
