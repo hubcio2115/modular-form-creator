7beb68b6432a5c004bedd882aba5ea303b283686
hubcio2115 <kowalski.hubercik1@gmail.com>
2026-07-12 20:10:48 +0200

    moved env.ts to src/ and replace full zod with zod/mini for smaller bundle size

--- apps/frontend/src/lib/$fetch.ts
+++ apps/frontend/src/lib/$fetch.ts
@@ -1,5 +1,5 @@
 import { createFetch } from "@better-fetch/fetch";
-import { env } from "@lib/env";
+import { env } from "~/env";
 
 export const $fetch = createFetch({
   baseURL: env.VITE_API_URL,
